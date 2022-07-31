7e8f843b03863bebce3f9660ad73456cbd185377
Rhama Fahri Afriansyah <rhamafahri17@gmail.com>
2022-07-31 20:28:28 +0700

    lanjutan-1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Parent-box">
+      <FotoProduk />
+      <ProdukInfo category="HOLIDAY" name="Ouraaaa" price="700.000" />
     </div>
   );
+}
+
+function FotoProduk() {
+  return (
+    <div className="Foto">
+      <img src="https://cdn.shopify.com/s/files/1/0054/6737/3638/products/rug-1650605555437-4_large.jpg?v=1650616222" />
+    </div>
+  );
+}
+
+function ProdukInfo(props) {
+  const { category, name, price } = props;
+
+  return (
+    <div className="Deskripsi">
+      <p className="Cate">{category}</p>
+      <h1 className="Title">{name}</h1>
+      <p className="Price">IDR {price}</p>
+      <p className="Info">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta culpa,
+        iste voluptatem minima dolorum tempore voluptate! Nihil maiores
+        consequatur tempora eum laudantium architecto? Aspernatur ipsa laborum
+        cum! Dolorem veritatis iure assumenda animi harum officia odit beatae,
+        distinctio qui sit quas obcaecati laborum atque ad architecto magni unde
+        inventore excepturi quidem!
+      </p>
+      <a onClick={(e) => TambahCart(name, e)} href="#">
+        Add Cart
+      </a>
+    </div>
+  );
+}
+
+function TambahCart(e) {
+  return console.log("Membeli produk " + e);
 }
 
 export default App;
