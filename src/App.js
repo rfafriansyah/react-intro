c1a4cb51146461c2c8366bccef0810a5cbcc24a4
Rhama Fahri Afriansyah <rhamafahri17@gmail.com>
2022-07-31 22:19:34 +0700

    sudah menambahkan check diskon, list, serta update produk info

--- src/App.js
+++ src/App.js
@@ -5,11 +5,17 @@ function App() {
   return (
     <div className="Parent-box">
       <FotoProduk />
-      <ProdukInfo category="HOLIDAY" name="Ouraaaa" price="700.000" />
+      <ProdukInfo
+        category="HOLIDAY"
+        name="Ouraaaa"
+        price="700.000"
+        isDiscount="coming"
+      />
     </div>
   );
 }
 
+// (Start) Foto Produk
 function FotoProduk() {
   return (
     <div className="Foto">
@@ -17,15 +23,26 @@ function FotoProduk() {
     </div>
   );
 }
+// (End)) Foto Produk
 
+// (Start) Produk Info
 function ProdukInfo(props) {
-  const { category, name, price } = props;
+  const { category, name, price, isDiscount } = props;
+  const benefits = [
+    "Tahan Air",
+    "Material Halus",
+    "Tidak Gerah",
+    "Tidak Kusut",
+    "Tahan Lama",
+  ];
+  const listBenefits = benefits.map((e) => <li>{e}</li>);
 
   return (
     <div className="Deskripsi">
       <p className="Cate">{category}</p>
       <h1 className="Title">{name}</h1>
       <p className="Price">IDR {price}</p>
+      <CheckDiscount isDiscount={isDiscount} />
       <p className="Info">
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta culpa,
         iste voluptatem minima dolorum tempore voluptate! Nihil maiores
@@ -34,15 +51,32 @@ function ProdukInfo(props) {
         distinctio qui sit quas obcaecati laborum atque ad architecto magni unde
         inventore excepturi quidem!
       </p>
+      <ul>{listBenefits}</ul>
       <a onClick={(e) => TambahCart(name, e)} href="#">
         Add Cart
       </a>
     </div>
   );
 }
+// (End) Produk Info
 
+// (Start) Check Diskon
+function CheckDiscount(props) {
+  const { isDiscount } = props;
+  if (isDiscount == "yes") {
+    return <p>Discount 50% Off</p>;
+  } else if (isDiscount == "coming") {
+    return <p>Discount is coming soon...</p>;
+  } else {
+    return <p>Nett</p>;
+  }
+}
+// (End) Check Diskon
+
+// (Start) Tambah Chart -Aksi-
 function TambahCart(e) {
   return console.log("Membeli produk " + e);
 }
+// (End) Tambah Chart -Aksi-
 
 export default App;
